e75b996de87b3a62c7ad05ca549d5d22c8f4f982
Sandra Grahl <grahlsan@gmail.com>
2021-08-17 11:58:03 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "reelfeminism",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
